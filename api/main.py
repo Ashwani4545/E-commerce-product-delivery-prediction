5c5256eef100f82bdedfc384d55963151e5ea608
Ashwani4545 <ashwanipandey4545@gmail.com>
2026-01-31 15:25:08 +0530

    Add health check, CORS, and loogging to FastAPI

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from api.schemas import PredictionInput
 from api.model import load_model
+
 import pandas as pd
 import logging
 
